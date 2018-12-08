1ac9e3ee7c08c4e5ddaaa112d23eb76a4696e121
Nehali Shinde <37983275+NehaliS@users.noreply.github.com>
2018-12-07 22:18:41 -0500

    Data Augmentation
    The augmentation factor : 360
    The code rotates each image from 1 degrees to 180 degrees (included) and then flips each rotated image horizontally.
    Libraries used :
    glob
    os
    numpy
     scipy , ndarray
    skimage
    skimage.util
    matplotlib.pyplot
    tensorflow
    math
    PIL
    
    
    To run the code : Change the source path to where the original images are stored.
    
    To change the augmentation factor : Change the variable 'i' which represents the degree of rotation.

--- dataaugment1.py
+++ dataaugment1.py
@@ -13,14 +13,12 @@ import math
 from PIL import Image
 
 sess = tf.InteractiveSession()
-print(glob.glob("/Users/shind/OneDrive/Second Semester/Analytical Data Mining/Project"))
+print(glob.glob("/Users/shind/OneDrive/Second Semester/Analytical Data Mining/Project")) #The path to the folder of images
 
-source_path = 'Alopecia_areata_images'
+source_path = 'Alopecia_areata_images' #Image folder
 desired_copies = 1000
 
 images = [os.path.join(source_path, f) for f in os.listdir(source_path) if os.path.isfile(os.path.join(source_path, f))]
-
-
 
 new_copies = 0
 
@@ -36,15 +34,13 @@ img = mpimg.imread(images[1])
 img1 = np.fliplr(img)   # To flip the image horizontally. Augmentation factor = 2x
 
 
-
-
 #With Tensorflow:
 
 '''img =plt.imread(images[1])
 tf_img = tf.convert_to_tensor(img)
 flip1_img = tf.image.flip_left_right(tf_img) #flip '''
 
-#brght_img = tf.image.adjust_brightness(tf_img, delta= 0.2) #brightness
+#brght_img = tf.image.adjust_brightness(tf_img, delta= 0.2) #brightness change
 
 
 img_dimension = tf.shape(img).eval()
@@ -53,15 +49,15 @@ original_size = [height, width, channel]
 x = tf.placeholder(dtype = tf.float32, shape = original_size)
 
 for image in images:
-    for i in range(2,181):
+    for i in range(1,181): 
         new_image = tf.image.decode_jpeg(tf.read_file(images[1]), channels=3)
         new_imager = tf.contrib.image.rotate(new_image, math.radians(i))
         new_imagef = tf.image.flip_left_right(new_imager)
         new_imager = tf.image.encode_jpeg(new_imager)
         new_imagef = tf.image.encode_jpeg(new_imagef)
         with tf.Session():
-            tf.write_file("rotate"+str(i)+".jpeg", new_imager).run()
-            tf.write_file("flip"+str(i)+".jpeg", new_imagef).run()
+            tf.write_file("rotate"+str(i)+".jpeg", new_imager).run() # augmentation factor : 180
+            tf.write_file("flip"+str(i)+".jpeg", new_imagef).run() # augmentation factor : 180
 
 
 
